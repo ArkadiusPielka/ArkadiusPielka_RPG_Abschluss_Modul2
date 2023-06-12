6face1b042c6b145e527ca9773a98bdf5c11fdb7
arkadiuspielka <akki1980@hotmail.de>
2023-06-12 17:30:17 +0200

    krieger angefangen

--- src/main/kotlin/Helden/Warrior.kt
+++ src/main/kotlin/Helden/Warrior.kt
@@ -18,9 +18,7 @@ class Warrior(level: Int, hp: Int) : Hero(hp, level) {
     init {
         println("Bitte geben sie den Namen für Ihren Krieger ein")
         this.name = readln()
-
     }
-
 
     val warriorAtk = mutableMapOf<String, Int>(
         "Hieb" to dmg * 2,
@@ -33,27 +31,60 @@ class Warrior(level: Int, hp: Int) : Hero(hp, level) {
         println("--- Krieger ist an der Reihe ---")
         println("Name: ${this.name}\tLevel: ${this.startLevel}")
         println("HP: ${this.startHP}/${this.maxHP}\tWut: ${this.rage}/${this.maxRage}")
-        println()
-        println("Sie haben folgende Optionen")
-        println("1 - ${atkNamen.elementAt(0)}")
-        println("2 - ${atkNamen.elementAt(1)}")
-        println("3 - ${atkNamen.elementAt(2)}")
-        println("4 - Beutel")
 
-        var eingabe = readln()
-        var index = eingabe.toInt() - 1
+        var eingabe: Int = 0
+        do {
+            println()
+            println("Sie haben folgende Optionen")
+            println("1 - ${atkNamen.elementAt(0)}")
+            println("2 - ${atkNamen.elementAt(1)}")
+            println("3 - ${atkNamen.elementAt(2)}")
+            println("4 - Beutel")
 
-        var attacke = atkNamen.elementAt(index)
-        println("Sie haben $attacke gewählt.")
-        println("'${this.name}' greift '${boss.name}' mit '$attacke' an")
+            try {
+                eingabe = readln().toInt()
+                var index = eingabe - 1
 
-        boss.bossHP -= warriorAtk[attacke]!!
+                var attacke = atkNamen.elementAt(index)
+                when (eingabe) {
 
-        println("--- ${boss.name} erleidet schaden ${warriorAtk[attacke]}---")
-        Thread.sleep(SLEEP_TIME / 2)
-        println("Name: ${boss.name}\t")
-        println("HP: ${boss.bossHP}/${boss.bossMaxHP}")
+                    1, 2 , 3 -> {
+                        println("Sie haben $attacke gewählt.")
+                        println("'${this.name}' greift '${boss.name}' mit '$attacke' an")
+                        boss.bossHP -= warriorAtk[attacke]!!
+                    }
 
+//                    2 -> {
+//                        println("Sie haben $attacke gewählt.")
+//                        println("'${this.name}' greift '${boss.name}' mit '$attacke' an")
+//                        boss.bossHP -= warriorAtk[attacke]!!
+//                    }
+//
+//                    3 -> {
+//                        println("Sie haben $attacke gewählt.")
+//                        println("'${this.name}' greift '${boss.name}' mit '$attacke' an")
+//                        boss.bossHP -= warriorAtk[attacke]!!
+//                    }
+
+                    4 -> {
+//                TODO Beutel einfügen
+                    }
+
+                    else -> {
+                        println("Falsche Eingabe")
+                    }
+
+                }
+
+                println("--- ${boss.name} erleidet schaden ${warriorAtk[attacke]}---")
+                Thread.sleep(SLEEP_TIME / 2)
+                println("Name: ${boss.name}\t")
+                println("HP: ${boss.bossHP}/${boss.bossMaxHP}")
+
+            } catch (e: Exception) {
+                println("Falsche Eingabe")
+            }
+
+        } while (eingabe != 1 && eingabe != 2 && eingabe != 3 && eingabe != 4)
     }
-//    TODO: Beutel anlegen, die anderen helden generieren
 }
